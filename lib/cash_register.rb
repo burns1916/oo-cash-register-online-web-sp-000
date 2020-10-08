a97955c7f5f6221785d961b25ef605e9aa4f5275
Ryan K Burns <ryankburns@gmail.com>
2020-10-08 16:52:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,6 +2,7 @@
 class CashRegister
 
   attr_accessor :total, :discount
+  @@item_array = []
 
   def initialize(discount = nil)
     @total = 0
@@ -12,8 +13,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 0)
-    @item_array = []
-    @item_array << item
+    @@item_array << item
     if quantity != 0
       self.total += price*quantity
     else
@@ -31,7 +31,7 @@ class CashRegister
   end
 
   def items
-    @item_array
+    @@item_array
   end
 
 end
