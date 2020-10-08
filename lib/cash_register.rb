d7828c55b8ed4ab157b67ebec6b727012d0884f5
Ryan K Burns <ryankburns@gmail.com>
2020-10-08 16:55:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 0)
-    @item_array << item*quantity
+    @item_array << item*quantity.split("")
     if quantity != 0
       self.total += price*quantity
     else
